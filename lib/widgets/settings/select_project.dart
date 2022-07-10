d57be95f632a801666c166b2b97b0460478310b7
Jesper Paulsen <jesper@jesper.no>
2022-07-10 13:17:26 +0200

    fix bug where id was used as path for project

--- lib/widgets/settings/select_project.dart
+++ lib/widgets/settings/select_project.dart
@@ -80,7 +80,7 @@ class SelectProject extends ConsumerWidget {
                   <InternalProjectConfig>[])
                 GestureDetector(
                   onTap: () =>
-                      _changePath(project.id, reloadTranslations, context),
+                      _changePath(project.path, reloadTranslations, context),
                   child: DecoratedBox(
                     decoration: const BoxDecoration(
                       border: Border(
